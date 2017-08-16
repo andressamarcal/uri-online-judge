9b5addf025678b04fe79a194f685395957a99356
andressamarcal <andressagb@gmail.com>
2017-08-15 23:11:12 -0300

    URI_2172

---
+++ URI_2172.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+   unsigned long long int X, M;
+   
+    while(1){
+        scanf("%lld %lld", &X, &M);
+        if (X==0 && M==0)
+            break;
+            printf("%lld\n", X*M);
+    }
+    return 0;
+}
